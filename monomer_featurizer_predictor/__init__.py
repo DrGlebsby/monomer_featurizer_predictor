62188b7e2d389f58565e4c0cdf7b760d293d7d04
Gleb Averochkin <glebaver@gmail.com>
2025-10-07 17:11:48 +0300

    Create __init__.py

---
+++ monomer_featurizer_predictor/__init__.py
@@ -0,0 +1,8 @@
+from .monomer_featurizer_predictor import (
+    featurize,
+    featurize_to_rus_dict,
+    featurize_to_eng_dict,
+    predict_targets
+)
+
+__all__ = ["featurize", "featurize_to_rus_dict", "featurize_to_eng_dict", "predict_targets"]
