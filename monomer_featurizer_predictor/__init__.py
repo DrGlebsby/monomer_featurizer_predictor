6b6f202133bf920782afd71bddd0fbc9c7b48380
Gleb Averochkin <glebaver@gmail.com>
2025-10-16 11:16:30 +0300

    Delete monomer_featurizer_predictor directory

--- monomer_featurizer_predictor/__init__.py
+++
@@ -1,8 +0,0 @@
-from .monomer_featurizer_predictor import (
-    featurize,
-    featurize_to_rus_dict,
-    featurize_to_eng_dict,
-    predict_targets
-)
-
-__all__ = ["featurize", "featurize_to_rus_dict", "featurize_to_eng_dict", "predict_targets"]
